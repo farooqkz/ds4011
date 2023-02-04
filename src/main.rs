1515fe65d44a099bdf16343d698147e2740cbca4
Farooq Karimi Zadeh <fkz@riseup.net>
2023-02-04 13:09:38 +0330

    up

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,29 @@ mod datastructures;
 mod consts;
 
 use std::io::stdin;
+use crate::datastructures::{VehicleSet, CustomerQueue};
+use crate::customer::Customer;
+use crate::vehicle::Vehicle;
 
 fn main() {
     let mut s = String::new();
-    stdin().read_line(&mut s).expect("wot?");
+    let mut vehicles = VehicleSet::new();
+    let mut number_of_vehicles: usize = 0;
+    while s != "{" {
+        stdin().read_line(&mut s).expect("wot?");
+        if let Err(error) = vehicles.add(Vehicle::new_from_string(s.clone(), number_of_vehicles)) {
+            println!("Error: {}", error);
+        } else {
+            println!("Added");
+        }
+        number_of_vehicles += 1;
+    }
+
+    while s != "}" {
+        let customers = CustomerQueue::new(100);
+        stdin().read_line(&mut s).expect("wot?");
+        
+    }
+
     println!("h {}", s);
 }
